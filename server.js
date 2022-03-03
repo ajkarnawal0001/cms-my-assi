315a40d41c6a82e68634b9f4fac9eb96e17da2a0
Amarjeet <ajkarnawal0001@gmail.com>
2022-03-03 11:08:19 +0530

    my project

--- server.js
+++ server.js
@@ -28,9 +28,6 @@ app.use("/file", fileController)
 if(process.env.NODE_ENV == "production"){
     app.use(express.static("client/build"))
 }
-app.get('*',(req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-});
 
 const start = async () => {
     await connect()
